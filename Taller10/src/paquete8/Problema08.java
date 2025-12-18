05569c9da26f123d0c274740433c3a145c2cdd83
jdguerrero28 <jdiegogueal@gmail.com>
2025-12-18 14:50:38 -0500

    Problema08

--- Taller10/src/paquete8/Problema08.java
+++ Taller10/src/paquete8/Problema08.java
@@ -9,5 +9,32 @@ package paquete8;
  * @author UTPL
  */
 public class Problema08 {
-    
+
+    public static void main(String[] args) {
+        double[][] dato1 = {{1, 2, 3}, {6, 8, 9}};
+        double[][] dato2 = {{10, 1, 2}, {10, 9, 1}};
+        double[][] dato3 = new double[2][3];
+        String reporte = "";
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (dato1[i][j] < dato2[i][j]) {
+                    dato3[i][j] = dato1[i][j];
+                } else {
+                    dato3[i][j] = dato2[i][j];
+
+                }
+
+            }
+
+        }
+        reporte = String.format("MATRIZ FINAL:\n");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                reporte = String.format("%s%.0f\t", reporte, dato3[i][j]);
+            }
+            reporte = String.format("%s\n",reporte);
+        }
+            System.out.printf("%s", reporte);        
+    }
 }

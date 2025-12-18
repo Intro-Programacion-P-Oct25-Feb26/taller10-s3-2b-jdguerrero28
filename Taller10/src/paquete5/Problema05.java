3f244fb07d8fe23cfb02312f8e5fd5670abd99b1
jdguerrero28 <jdiegogueal@gmail.com>
2025-12-18 12:24:40 -0500

    Problema05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,32 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {
+            {"Sarah Ward", "Philip Payne"},
+            {"Carrie Burton", "Lauren Rice"},
+            {"Emma Escobar", "Lori Flores"},
+            {"Steven West", "Toni Martin"}
+        };
+
+        String reporte = "";
+
+        reporte = String.format("Estudiantes que inician con S, P o T:\n");
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+
+                String inicial = estudiantes[i][j].substring(0, 1);
+
+                if (inicial.equals("S") || inicial.equals("P")
+                        || inicial.equals("T")) {
+                    reporte = String.format("%s%s\n", reporte,
+                            estudiantes[i][j]);
+                }
+            }
+        }
+
+        System.out.printf("%s\n", reporte);
+    }
 }

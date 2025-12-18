a34564e7cbedba525d7169c1425fc76c28755ad8
jdguerrero28 <jdiegogueal@gmail.com>
2025-12-18 12:46:22 -0500

    Nuevos Paquetes

---
+++ Taller10/src/paquete7/Problema07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete7;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Problema07 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
